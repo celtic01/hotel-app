8265dd4e54369e3751dc401dcda128965daf5b4e
celtic01 <andrei.bortas@student.unitbv.ro>
2024-06-12 18:18:42 +0300

    small stuff

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -780,7 +780,7 @@ func (m *Repository) AdminPostReservationsCalendar(w http.ResponseWriter, r *htt
 			}
 		}
 	}
-	for name, _ := range r.PostForm {
+	for name := range r.PostForm {
 		if strings.HasPrefix(name, "add_block") {
 			exploded := strings.Split(name, "_")
 			roomID, _ := strconv.Atoi(exploded[2])
